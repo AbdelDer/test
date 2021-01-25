fa1f861c0277c4bcbbb62ae7ec8c3cb2e067c146
AbdelDer <AbdelDer@github.com>
2021-01-25 22:23:21 +0100

    add/edit, responsive design 100%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  editableResponsable : boolean[] = [false, false];
+  currentDate = new Date();
+  responsables : Responsable[] = [
+    {
+      "nom": "Scabandra Lbahria",
+      "role": "Directeur General",
+      "numero": "+125649763665",
+      "email": "scabandra@mail.com"
+    },
+    {
+      "nom": "Sridina Wld Lhay",
+      "role": "Directeur",
+      "numero": "+135485663557",
+      "email": "sridina@mail.com"
+    }
+  ];
+
+  change(event) {
+    console.log(this.currentDate);
+  }
+
+  addResponsable() {
+    this.responsables[this.responsables.length] = {
+      "nom" : "",
+      "email" : "",
+      "numero" : "",
+      "role" : ""
+    }
+  }
+}
+
+export interface Responsable {
+  nom: string;
+  role: string;
+  numero: string;
+  email: string;
 }
